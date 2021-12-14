c19c7e701487c7c160cd16f0519c1b803ce31c63
Grohmur <zverma92@gmail.com>
2021-12-14 22:33:51 +0300

    Login Function

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,22 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var userName: String {
+    var savedUserName: String {
         "user"
     }
-    var password: String {
+    var savedPassword: String {
         "password"
     }
+    @IBOutlet weak var enteredUserName: UITextField!
+    @IBOutlet weak var enteredPassword: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func forgetNamePressed() {
-        showAlert(messadge: "Your name is <\(userName)>")
+        showAlert(messadge: "Your name is <\(savedUserName)>")
     }
     @IBAction func forgetPasswordPressed() {
-        showAlert(messadge: "Your password is <\(password)>")
+        showAlert(messadge: "Your password is <\(savedPassword)>")
     }
     
 }
@@ -35,5 +37,22 @@ extension ViewController {
         let alert = UIAlertController(title: "Oops!", message: messadge, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default))
         present(alert, animated: true)
+    }
+    
+    private func wrongText(wrongTextField: String) {
+        let alert = UIAlertController(title: "Error", message: "\(wrongTextField) is wrong", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true)
+    }
+    
+    private func login(userName: String?, password: String?) {
+        guard let name = userName, name == savedUserName else { wrongText(wrongTextField: "User Name")
+            return
+        }
+        guard let pass = password, pass == savedPassword else {
+            wrongText(wrongTextField: "Password")
+            return
+        }
+        
     }
 }
