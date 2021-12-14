003d956e4d6e56355c66ead469ced56a0a750af3
Grohmur <zverma92@gmail.com>
2021-12-14 21:28:57 +0300

    Forget User name realized

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var userName: String {
+        "user"
+    }
+    var password: String {
+        "password"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func forgetUserNamePressed() {
+        let nameAlert = UIAlertController(title: "Oops!", message: "Your name is \(userName)", preferredStyle: .alert)
+        present(nameAlert, animated: true)
+    }
+    
 }
-
